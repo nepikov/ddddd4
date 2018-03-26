06f70f2e7256cbc91e963b2d71304f9904a10146
Denis <nepikov@yandex.ru>
2018-03-26 20:56:05 +0300

    12

--- ddddd4/AppDelegate.swift
+++ ddddd4/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  ddddd4
+//  ddddd4qwererwwqrrrrrrrrrrrrrrrrswdjk;fadkls;fjadkls;jfal;ksdfs;kdjnmmjmmmmµµµm
 //
 //  Created by  Denis on 26/03/2018.
 //  Copyright © 2018  Denis. All rights reserved.
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch.qwerqwerqwerqwerqwerqwerqwer
         return true
     }
 
